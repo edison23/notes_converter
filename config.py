c6cb723f0812defd6935ba86700339176ef95f30
edison23 <klementdavid@gmail.com>
2013-10-12 19:37:08 +0200

    First commit

---
+++ config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# configuration file for notes converted
+
+
+path = "h:/PLIN/03-sem"
+out_path = "h:/PLIN/output_notes"
+
+header = "./inc/notes_header.html"
+footer = "./inc/notes_footer.html"
+
+prefix = ".notes_-_"
+sufix = ".txt"
+new_prefix = "notes_-_"
+doc_title = u"Poznámky"
